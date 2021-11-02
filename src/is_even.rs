5c94c733f2b75eb759f4da7606d5bab1a68dc8ed
Bas Boot <b.boot@student.tudelft.nl>
2021-11-02 21:34:59 +0100

    Add unit test (is_even)

--- src/is_even.rs
+++ src/is_even.rs
@@ -1,3 +1,19 @@
 pub fn is_even(n: u64) -> bool {
     return (n % 2) == 0;
 }
+
+#[cfg(test)]
+mod tests {
+    // Note this useful idiom: importing names from outer (for mod tests) scope.
+    use super::*;
+
+    #[test]
+    fn test_even() {
+        assert_eq!(is_even(2), true);
+    }
+
+    #[test]
+    fn test_not_even() {
+        assert_eq!(is_even(1), false);
+    }
+}
