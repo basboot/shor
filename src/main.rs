2d23632671403284018b4b20bd13c8baec51bc82
Bas Boot <b.boot@student.tudelft.nl>
2021-11-02 13:54:14 +0100

    Add sieve of Eratosthenes

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+mod sieve_of_eratosthenes;
+use sieve_of_eratosthenes::sieve_of_eratosthenes;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Hello, Shor!");
+
+    let candidate: usize = 18;
+
+    let primes = sieve_of_eratosthenes();
+
+    if !primes.contains(&candidate) {
+        println!("{} is not prime (possible candidate)", candidate);
+    } else {
+        println!("{} is prime (so not a possible candidate)", candidate);
+    }
 }
---
+++ src/sieve_of_eratosthenes.rs
@@ -0,0 +1,36 @@
+use std::collections::HashSet;
+
+const N_SIEVE: usize = 1000; // size of the seive = max prime + 1
+
+/***
+    Find all primes < N_SIEVE using a sieve of Eratosthenes
+
+    return: set with primes
+ */
+pub fn sieve_of_eratosthenes() -> HashSet<usize>{
+    // Init sieve, mark every number as true (possible prime)
+    let mut sieve: [bool; N_SIEVE] = [true; N_SIEVE];
+    // Create empty set to return primes
+    let mut primes = HashSet::new();
+    // Add trivial prime to set
+    primes.insert(1);
+
+    // Use sieve to add non trivial primes to set
+    for i in 2..N_SIEVE {
+        // prime found
+        if sieve[i] {
+            // add prime to set
+            primes.insert(i);
+            //println!("Prime found {}", i);
+
+            // update sieve
+            let mut no_prime_number = i + i;
+            while no_prime_number < N_SIEVE {
+                sieve[no_prime_number] = false;
+                no_prime_number += i;
+            }
+        }
+    }
+
+    return primes;
+}
