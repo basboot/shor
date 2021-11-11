7bc9223bab99515a789065deb62763d6903f722b
Bas Boot <b.boot@student.tudelft.nl>
2021-11-11 17:30:43 +0100

    Add complex numbers to quantum register

--- src/quantum_register.rs
+++ src/quantum_register.rs
@@ -4,30 +4,30 @@ const N_BITS_REG2:usize = 3; // size of register 2, must have enough qubits to r
 const EPSILON: f64 = 0_f64;
 
 use ndarray::Array2;
+use num::complex::Complex;
 
 // create statusvector for a register that represents N_BITS_REG1+N_BITS_REG1 qbits
-pub fn create_quantum_register() -> Array2::<f64> {
+pub fn create_quantum_register() -> Array2::<Complex<f64>> {
     // TODO: use complex numbers
-    let mut register = Array2::<f64>::zeros((2_usize.pow((N_BITS_REG1 + N_BITS_REG2) as u32), 1));
+    let mut register = Array2::<Complex<f64>>::zeros((2_usize.pow((N_BITS_REG1 + N_BITS_REG2) as u32), 1));
 
     // set register to a valid state (zero)
-    register[[0, 0]] = 1.;
-
+    register[[0, 0]] = num::complex::Complex::new(1.0, 0.0);
     register
 }
 
-pub fn init_quantum_register(register: &mut Array2::<f64>) {
+pub fn init_quantum_register(register: &mut Array2::<Complex<f64>>) {
     let n_state_vectors_reg1 = 2_u32.pow(N_BITS_REG1 as u32);
     for i in 0..n_state_vectors_reg1 {
-        register[[(i as usize) << N_BITS_REG2, 0]] = 1./(n_state_vectors_reg1 as f64).sqrt();
+        register[[(i as usize) << N_BITS_REG2, 0]] = num::complex::Complex::new(1./(n_state_vectors_reg1 as f64).sqrt(), 0.0);
     }
 }
 
-pub fn print_quantum_register(register: &Array2::<f64>) {
-    println!("reg1 - reg2 (p)");
+pub fn print_quantum_register(register: &Array2::<Complex<f64>>) {
+    println!("reg1 - reg2 (value)");
     for i in 0..register.len() {
-        if register[[i, 0]].abs() > EPSILON {
-            println!("{} - {} ({})", i >> N_BITS_REG2, i as u32 & (2_u32.pow(N_BITS_REG2 as u32) - 1), register[[i, 0]].powf(2_f64));
+        if register[[i, 0]].norm() > EPSILON {
+            println!("{} - {} ({})", i >> N_BITS_REG2, i as u32 & (2_u32.pow(N_BITS_REG2 as u32) - 1), register[[i, 0]]);
         }
     }
 }
